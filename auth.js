8d5bd541e4c2fc1448032cf28049c25fd97dd33a
Ram Shreyas Naik Sabavat <rsns2012@outlook.com>
2024-12-07 08:30:46 -0800

    Updated script tags to use modules

--- auth.js
+++ auth.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore"; // Import Firestore functions
